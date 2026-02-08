1564955d814b9898e8e61fef61b20626ee0ed21f
Melih Topchuoglu <86005778+CarAnth@users.noreply.github.com>
2026-02-09 00:30:34 +0100

    Add request event to traffic light controller

--- traffic_light_controller.h
+++ traffic_light_controller.h
@@ -10,9 +10,10 @@ SC_MODULE(LightController) {
 
     // vehicle arrival events(events)
     //Reference: https://www.asic-world.com/systemc/systemc_time4.html#sc_event
-    sc_event ev_NS, ev_SN, ev_WE, ev_EW;// we're using sc_event rather than sc_signal<bool> because it's easier to use.
+    sc_event ev_NS, ev_SN, ev_WE, ev_EW, ev_req;// we're using sc_event rather than sc_signal<bool> because it's easier to use.
 
     bool req_NS, req_SN, req_WE, req_EW;//request flags, for the memory
+
 
     SC_HAS_PROCESS(LightController);//module has process
     LightController(sc_module_name name);
