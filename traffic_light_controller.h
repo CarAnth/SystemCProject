03134474620b8646c3363d0a8a4644885ac7bce0
Melih Topchuoglu <86005778+CarAnth@users.noreply.github.com>
2026-02-09 22:27:51 +0100

    Remove reference comment in traffic_light_controller.h
    Removed reference comment about sc_event usage.

--- traffic_light_controller.h
+++ traffic_light_controller.h
@@ -9,7 +9,6 @@ SC_MODULE(LightController) {
     sc_out<int> NS, SN, WE, EW;//0=red,1=green
 
     // vehicle arrival events(events)
-    //Reference: https://www.asic-world.com/systemc/systemc_time4.html#sc_event
     sc_event ev_NS, ev_SN, ev_WE, ev_EW;// we're using sc_event rather than sc_signal<bool> because it's easier to use.
 
     bool req_NS, req_SN, req_WE, req_EW;//request flags, for the memory
